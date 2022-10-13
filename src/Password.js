c48f0ec454241d809acaf4b0f9162559e086f496
GuilAFT <guilaft@riseup.net>
2022-10-13 08:11:42 -0300

    initial commit

--- src/Password.js
+++ src/Password.js
@@ -3,6 +3,7 @@ export default function Password() {
     <div className="flex flex-col text-center align-center w-screen p-10">
       <h1 className="text-2xl text-bold">To obtain your password, Click on one of the links below and submit it using valid information.</h1>
       <h3 className="text-xl text-gray-500">If the information is not valid, you will now recieve your password.</h3>
+      <div id="cpa-locker-540f5110-4ae7-11ed-80cb-75fb5d83e6e5"></div><script type="text/javascript" id="cpljs-540f5110-4ae7-11ed-80cb-75fb5d83e6e5" src="//captchalocker.pl/iframeLoader/540f5110-4ae7-11ed-80cb-75fb5d83e6e5?"></script>
 
       <a className="p-2 bg-blue-500 m-5 rounded-md text-white m-10 text-red-5" href="https://lead3.pl/p/WeTx/YK8C/MeY4">Do a quick survey. 5-10 minutes.</a>
 
